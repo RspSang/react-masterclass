7cce7de50cc31037ae40621999d0378f7522fed8
HealMon <71082215+RspSang@users.noreply.github.com>
2021-12-01 22:08:56 +0900

    Study Typescript (constainer)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="teal" borderColor="red" />
+      <Circle bgColor="tomato" text="this is circle" />
+    </div>
+  );
+}
+
+export default App;
